46b2d472c62af1c15ebbf570855e4fb87d6fd67b
viliam0206 <v.trclam@gmail.com>
2023-06-15 19:44:28 +0700

    Done CRUD tables.

---
+++ Infrastructures/IRepositories/ITableRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructures.IRepositories;
+
+public interface ITableRepository
+{
+    Task<IEnumerable<Table>> GetTablesAsync();
+    Task<Table?> GetTableByIdAsync(Guid id);
+    void AddTable(Table table);
+    void RemoveTable(Table table);
+}
---
+++ WebAPI/Models/TableModels/TableModel.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models.TableModels;
+
+public class TableModel
+{
+    [MaxLength(50)]
+    public string Code { get; set; } = null!;
+    public int SeatQuantity { get; set; } = 0;
+    public string Status { get; set; } = "Empty";
+}
